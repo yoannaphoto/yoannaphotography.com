1fc0df6aef429678127a0882928374a5e6659632
Nazar Ivaniv <nivaniv@gmail.com>
2013-07-21 21:56:15 -0400

    Added about and prices pages, changed home photos slider to vegas - full screen

--- js/theme.js
+++ js/theme.js
@@ -1,106 +1,99 @@
 var readyState = function (callback) {
-	var body = document.body;
+    var body = document.body;
 
-	if (body && body.readyState == 'loaded') {
-		callback();
-	}
-	else {
-		if (window.addEventListener) {
-			window.addEventListener('load', callback, false);
-		}
-		else {
-			window.attachEvent('onload', callback);
-		}
-	}
+    if (body && body.readyState == 'loaded') {
+        callback();
+    }
+    else {
+        if (window.addEventListener) {
+            window.addEventListener('load', callback, false);
+        }
+        else {
+            window.attachEvent('onload', callback);
+        }
+    }
 }
 
 readyState(function () {
-	/**
-	 * Scroll Page
-	 */
-	function scrollPage(page) {
-		$('#navigation a[data-nav="scroll"]').removeClass('active');
+    /**
+     * Scroll Page
+     */
+    function scrollPage(page) {
+        $('#navigation a[data-nav="scroll"]').removeClass('active');
 
-		$('#navigation a[href="#/' + page + '"]').addClass('active');
+        $('#navigation a[href="#' + page + '"]').addClass('active');
 
-		scroll = false;
+        scroll = false;
 
-		$('html, body').animate({ scrollTop:$('#' + page).offset().top }, 800, function () {
-			scroll = true;
-		});
-	}
+        $('html, body').animate({ scrollTop:$('#' + page).offset().top }, 800, function () {
+            scroll = true;
+        });
+    }
 
-	/**
-	 * Panel offset
-	 */
-	$('.panel').css({'margin-top':$('#header').height()});
+    /**
+     * Panel offset
+     */
+    $('.panel').css({'margin-top':$('#header').height()});
 
+    function introPos() {
+        $('#intro').css({'margin-top':( ($('.box:first').height() / 2) - $('#header').height() - ($('#intro').height() / 2) )});
+    }
 
-	/**
-	 * Window scroll
-	 */
-	$(window).scroll(function () {
-		var self = this;
-
-		if ($(this).scrollTop() > 100) {
-			$('#back-top').fadeIn();
-		}
-		else {
-			$('#back-top').fadeOut();
-		}
-	});
-
-	/**
-	 * Window resize
-	 */
-	$(window).resize(function () {
-		introPos();
-	});
-
-	/**
-	 * Scroll to top links
-	 */
-	$('#back-top').click(function (event) {
-		$('#navigation a[data-nav="scroll"]').removeClass('active');
-
-		var firstItem = $('#navigation a[data-nav="scroll"]:first');
-
-		firstItem.addClass('active');
-
-		hasher.setHash(firstItem.attr('href').replace('#/', ''));
-
-		$('html, body').animate({ scrollTop:0 }, 800);
-
-		return false;
-	});
-
-	$('#logo').click(function (event) {
-		$('#navigation a[data-nav="scroll"]').removeClass('active');
-
-		var firstItem = $('#navigation a[data-nav="scroll"]:first');
-
-		firstItem.addClass('active');
-
-		hasher.setHash(firstItem.attr('href').replace('#/', ''));
-
-		$('html, body').animate({ scrollTop:0 }, 800);
-
-		return false;
-	});
+    introPos();
+    $('#intro').fadeIn();
 
 
-	$("#slider4").responsiveSlides({
-		auto: true,
-		pager: false,
-		nav: true,
-		speed: 500,
-		namespace: "callbacks",
-		before: function () {
-			$('.events').append("<li>before event fired.</li>");
-		},
-		after: function () {
-			$('.events').append("<li>after event fired.</li>");
-		}
-	});
+    /**
+     * Window scroll
+     */
+    $(window).scroll(function () {
+        var self = this;
 
+        if ($(this).scrollTop() > 100) {
+            $('#back-top').fadeIn();
+        }
+        else {
+            $('#back-top').fadeOut();
+        }
+    });
+
+    /**
+     * Window resize
+     */
+    $(window).resize(function () {
+        introPos();
+    });
+
+    /**
+     * Scroll to top links
+     */
+    $('#navigation a[data-nav="scroll"]:last').click(function (event) {
+        $('html, body').animate({ scrollTop:$('#contact').offset().top }, 800);
+        return false;
+    });
+
+    $('#back-top').click(function (event) {
+//        $('#navigation a[data-nav="scroll"]').removeClass('active');
+//        var firstItem = $('#navigation a[data-nav="scroll"]:first');
+//        firstItem.addClass('active');
+
+        $('html, body').animate({ scrollTop:0 }, 800);
+        return false;
+    });
+
+
+    /**
+     * Vegas background image slider
+     */
+    if (vegas_images.length) {
+        $.vegas('slideshow', {
+            delay:10000,
+            backgrounds:vegas_images
+        })('overlay');
+    }
+
+    /**
+     * Flexslider
+     */
+    $('.flexslider').flexslider();
 });
